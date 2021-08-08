f182e35d7bb0661374138ffc6e80d7d84a12829e
jie yang <topelinux@gmail.com>
2021-08-08 21:52:35 +0800

    update for convert

--- src/aht20.rs
+++ src/aht20.rs
@@ -45,6 +45,19 @@ impl error::Error for Aht20Error {}
 //    }
 //}
 
+pub trait Aht20Decoder {
+    fn to_human(&self) -> (f32, f32);
+}
+
+impl Aht20Decoder for [u8;6] {
+    fn to_human(&self) -> (f32, f32) {
+        let humid_data:u32 = (self[1] as u32) << 12 | (self[2] as u32) << 4 | (self[3] as u32) & 0xF0 >> 4;
+        let temp_data:u32 = (self[3] as u32 & 0xF)<< 16 | (self[4] as u32) << 8 | self[5] as u32;
+
+        (humid_data as f32 / 2_i32.pow(20) as f32 * 100.0, temp_data as f32 * ((200.0)/2_i32.pow(20) as f32) - 50.0)
+    }
+}
+
 impl Aht20 {
     pub fn new(bus: u8, addr: u16) -> Result<Aht20, Aht20Error> {
         let mut i2c = I2c::with_bus(bus)?;
@@ -77,22 +90,18 @@ impl Aht20 {
         let (humid_data, temp_data) =
         {
             loop {
-                thread::sleep(Duration::from_micros(100));
+                thread::sleep(Duration::from_millis(100));
                 let status = self.get_status()?;
                 if (status & 0x80u8 == 0) {
                     self.i2c.read(&mut reg)?;
-                    println!("data is {:?}", reg);
-                    let humid_data:u32 = (reg[0] as u32) << 12 | (reg[1] as u32) << 4 | (reg[2] as u32) & 0xF0 >> 4;
-                    let temp_data:u32 = (reg[3] as u32 & 0xF)<< 16 | (reg[4] as u32) << 8 | reg[5] as u32;
-
-                    break (humid_data, temp_data);
+                    break reg.to_human();
                 } else {
                     println!("invalid status {}", status);
                 }
             }
         };
-        println!("humid_data is {} temp data is {}", humid_data, temp_data as f32 *((200.0)/2_i32.pow(20) as f32) - 50.0);
-        Ok((5.0, 6.0))
+        println!("humid_data is {} temp data is {}", humid_data, temp_data);
+        Ok((humid_data, temp_data))
     }
 
     pub fn init(&mut self) -> Result<(), Aht20Error> {
